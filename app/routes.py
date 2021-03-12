d2fdab9d7dc8265cd258728bc3d97cfb3b7ef3cf
esqning <esqning@aliyun.com>
2021-03-12 21:41:46 +0800

    url_for()补充一行

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginFrom
 
 
@@ -26,5 +26,5 @@ def login():
     if form.validate_on_submit():
         msg = 'Login requested for user {},remember_me={}'.format(form.username.data, form.remember_me.data)
         flash(msg)
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='login', form=form)
