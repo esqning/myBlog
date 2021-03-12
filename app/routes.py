875de46bdf80c359446655475f35d4a0b3f38a76
esqning <esqning@aliyun.com>
2021-03-12 19:38:55 +0800

    使用模板

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,9 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'ning'}
-    return '''
-<html>
-    <head>
-        <title> Home Page -myBlog </title> 
-    <head>
-    <body>
-        <h1>Hello,'''+user['username']+'''!</h1>
-    </body>
-</html>     
-'''
+    return render_template('index.html', title='home', user=user)
