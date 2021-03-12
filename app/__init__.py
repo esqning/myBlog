9f39569a7527a7ac285b8627f11c2d44ed15eeb4
esqning <esqning@aliyun.com>
2021-03-12 20:34:59 +0800

    使用配置文件

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+from config import Config
 app = Flask(__name__)
+app.config.from_object(Config)
 
-print("使用者", __name__)
+# print("使用者", __name__)
+print(app.config['SECRET_KEY'])
 
 # 因为python存在循环引用问题，所以此导入不能写在前面
 from app import routes
