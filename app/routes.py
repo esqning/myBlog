cb52f5602713d3337902ea2017d65000f6aa44c4
esqning <esqning@aliyun.com>
2021-03-12 21:30:42 +0800

    使用表单并使用闪现消息

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginFrom
 
 
 @app.route('/')
@@ -17,3 +18,13 @@ def index():
         }
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginFrom()
+    if form.validate_on_submit():
+        msg = 'Login requested for user {},remember_me={}'.format(form.username.data, form.remember_me.data)
+        flash(msg)
+        return redirect('/index')
+    return render_template('login.html', title='login', form=form)
