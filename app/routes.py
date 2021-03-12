7e6fde44849b4b681c86234ce7346b46c9d41d11
esqning <esqning@aliyun.com>
2021-03-12 18:35:30 +0800

    不使用模板

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'ning'}
+    return '''
+<html>
+    <head>
+        <title> Home Page -myBlog </title> 
+    <head>
+    <body>
+        <h1>Hello,'''+user['username']+'''!</h1>
+    </body>
+</html>     
+'''
