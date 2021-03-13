a202ed5fa40b9ff033279db0092ab92a711fe592
esqning <esqning@aliyun.com>
2021-03-13 15:35:43 +0800

    添加密码加密以及UserMixin混入类

--- app/model.py
+++ app/model.py
@@ -1,14 +1,22 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 # 如果是一对多的关系 那就需要在多的那端创建一个外键，他是对的那端的用户的主键
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
